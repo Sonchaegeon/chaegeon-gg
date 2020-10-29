9b2f96b821bf804a9f48947b76da7315e5a8a43f
Chaegeon <thscorjs1002@naver.com>
2020-10-29 09:43:19 +0900

    apikey

--- models/league.js
+++ models/league.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-var api_key = "RGAPI-52f2327e-972f-4d74-910a-f5ebf91d21e3";
+var api_key = "RGAPI-65c858de-4601-4596-8835-f560dd0d404c";
 module.exports = {
     SummonerName: async (req) => {
         var summonerName = (encodeURI(req.query.name));
